2b0e5489357d1e1ea96e7c9413f0f6c2f0c5884a
Arliezv <rigiemessarevano@gmail.com>
2026-08-02 22:28:47 +0700

    Update belajar_variabel and remove asiggnment

--- belajar_variabel.py
+++ belajar_variabel.py
@@ -3,3 +3,6 @@ b = 23
 c = 50
 
 print("nilai a adalah: ", a)
+print("nilai b adalah: ", b)
+print("nilai c adalah: ", c)
+
---
+++ operator_assignment.py
@@ -0,0 +1,8 @@
+a = 5 #ini adalah assigment
+print("nilai = a",a)
+
+a = a+1
+print("nilai a =",a)
+
+a += 4
+print("nilai = a",a)    
